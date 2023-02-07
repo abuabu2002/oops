427f27e568ef587706b9532406bab37703b9c76b
abuabu2002 <124653179+abuabu2002@users.noreply.github.com>
2023-02-07 10:47:15 +0530

    finding area of square

---
+++ AREAOFSQUARE.java
@@ -0,0 +1,13 @@
+//Getting value of a from the user and printing area of square
+import java.io.*;
+import java.util.*;
+public class AREAOFSQUARE
+{
+public static void main(String args[])
+{
+Scanner S=new Scanner(System.in);System.out.println("enter the value of a:");
+int a= S.nextInt();
+int AREAOFSQUARE=a*a;
+System.out.println("area of square"+" "+AREAOFSQUARE);
+}
+}
