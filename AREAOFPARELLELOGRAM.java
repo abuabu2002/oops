50335c90914569872fd66933280fadbf2e683692
abuabu2002 <124653179+abuabu2002@users.noreply.github.com>
2023-02-07 10:41:42 +0530

    finding area of parallelogram

---
+++ AREAOFPARELLELOGRAM.java
@@ -0,0 +1,14 @@
+//Getting value of length and breath from the user and printing area of parallelogram
+import java.io.*;
+import java.util.*;
+public class AREAOFPARELLELOGRAM
+{
+public static void main(String args[])
+{
+Scanner S=new Scanner(System.in);System.out.println("enter the value of length l=:");
+int l= S.nextInt();System.out.println("enter the value of breath b=:");
+int b= S.nextInt();
+int AREAOFPARELLELOGRAM=l*b;
+System.out.println("area of parallelogram="+" "+AREAOFPARELLELOGRAM);
+}
+}
