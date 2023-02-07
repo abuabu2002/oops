91f2771d93b6b811946fd264244358e5e5727add
abuabu2002 <124653179+abuabu2002@users.noreply.github.com>
2023-02-07 10:33:12 +0530

    addition of an array

---
+++ Addingarray.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.util.Arrays;
+public class Addingarray{
+public static void main (String args[])
+{
+int[] age={12,4,5,2,5};
+int n=age.length;
+int multi =0;
+System.out.println("Enter the elements of array: ");
+System.out.println("First element"+age[0]);
+System.out.println("Second element "+age[1]);
+System.out.println("Third element"+age[2]);
+System.out.println("Fourth element"+age[3]);
+System.out.println("Fifth element"+age[4]);
+
+for (i=0;i<n;i++)
+{
+multi=multi+a(i);
+}
+System.out.println("the multiplication of given elements:"+multi);
+
+
+
+
+}
+}
